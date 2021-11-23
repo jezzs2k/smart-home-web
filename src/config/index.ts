1c5a921bce178a765cf2540aa0683b9b8c83ff78
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-11-23 14:40:52 +0700

    change port

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ import Colors from './Colors';
 
 const RADIUS_DEFAULT_INPUT_BTN = 8;
 
-const URL_DEV = 'http://invoice-maker.tech:8080';
+const URL_DEV = "http://invoice-maker.tech:8081";
 
 export {Colors, RADIUS_DEFAULT_INPUT_BTN, URL_DEV};
